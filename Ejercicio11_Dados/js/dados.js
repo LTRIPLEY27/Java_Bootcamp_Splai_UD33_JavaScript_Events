5f76a899ed3c61f458665e87ca2c445805569d84
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-03-09 10:08:48 +0100

    review

--- Ejercicio11_Dados/js/dados.js
+++ Ejercicio11_Dados/js/dados.js
@@ -1,7 +1,23 @@
-function calculum () {
-    var dado1 = parseInt((Math.random() * (6 - 1)) + 1);
-    var dado2 = parseInt((Math.random() * (6 - 1)) + 1);
+/** GLOBAL VARIABLES */
 
+var dado1;
+var dado2;
+var total;
+var sumaDados;
+
+function calculum () {
+    dado1 = parseInt((Math.random() * (6 - 1)) + 1);
+    dado2 = parseInt((Math.random() * (6 - 1)) + 1);
     document.getElementById("dados").innerHTML+= "Los números del dado 1 son : " + dado1;
     document.getElementById("dados").innerHTML+= "Los números del dado 2 son : " + dado2;
+    total = parseInt(dado1 + dado2);
+
+    sumaDados += [total];
+    document.getElementById("total").innerHTML+= "La suma es igual a" + sumaDados[total];
+}
+
+function sumaDado(){
+    for(let i = 0; i < sumaDados; i++){
+        document.getElementById("total").innerHTML+= "La suma es igual a" + sumaDados[i];
+    }
 }
