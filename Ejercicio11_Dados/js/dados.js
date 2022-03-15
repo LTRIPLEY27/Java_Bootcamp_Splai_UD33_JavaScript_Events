e98719b6f9105296c8a96e747c7330ffad6b1b1f
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-03-15 19:01:16 +0100

    ultimando detalles

--- Ejercicio11_Dados/js/dados.js
+++ Ejercicio11_Dados/js/dados.js
@@ -2,22 +2,63 @@
 
 var dado1;
 var dado2;
-var total;
-var sumaDados;
+var sumaDados = [0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0]; //SOLO 11 POSIBLES SUMAS
 
-function calculum () {
+function calculum() {
     dado1 = parseInt((Math.random() * (6 - 1)) + 1);
     dado2 = parseInt((Math.random() * (6 - 1)) + 1);
-    document.getElementById("dados").innerHTML+= "Los números del dado 1 son : " + dado1;
-    document.getElementById("dados").innerHTML+= "Los números del dado 2 son : " + dado2;
-    total = parseInt(dado1 + dado2);
+    document.getElementById("dados1").innerHTML+= "Los números del dado 1 son : " + dado1;
+    document.getElementById("dados2").innerHTML+= "Los números del dado 2 son : " + dado2;
+    total = dado1 + dado2;
+    alert(total);
+    document.getElementById("suma").innerHTML+= "La suma es igual a" + total;
 
-    sumaDados += [total];
-    document.getElementById("total").innerHTML+= "La suma es igual a" + sumaDados[total];
-}
+    //LLAMADO AL MÉTODO DE VERIFICACIÓN DE LA REPETICIÓN DE SUMAS
 
-function sumaDado(){
-    for(let i = 0; i < sumaDados; i++){
-        document.getElementById("total").innerHTML+= "La suma es igual a" + sumaDados[i];
+    for(let i = 0; i < 36000; i++){
+        switch(total) {
+            case 2://CONTEO DE REPETICIÓN DE LAS SUMAS POR CADA CASE
+                sumaDados[0]++; 
+                break;
+            case 3:
+                sumaDados[1]++;
+                break;
+            case 4:
+                sumaDados[2]++;
+                break;
+            case 5:
+                sumaDados[3]++;
+                break;
+            case 6:
+                sumaDados[4]++;
+            break;
+            case 7:
+                sumaDados[5]++;
+                break;
+            case 8:
+                sumaDados[6]++;
+                break;
+            case 9:
+                sumaDados[7]++;
+                break;
+            case 10:
+                sumaDados[8]++;
+                break;
+            case 11:
+                sumaDados[9]++;
+                break;
+            case 12:
+                sumaDados[10]++;
+                break;    
+        }
     }
+
+    iterador = 2;
+    //REPETICIONES DE CADA APARICION, EN CASO DE HACER NÚMEROS AL AZAR
+
+    for(let i = 0; i < sumaDados.length; i++) {
+        console.log("La suma de " + iterador +" se ha repetido " + sumaDados[i] + "veces");
+        iterador++;
+    }
+
 }
--- Ejercicio12_REGEX/js/expresiones.js
+++ Ejercicio12_REGEX/js/expresiones.js
@@ -1,6 +1,15 @@
-var expresion = prompt("Ingresa expresión");
-var validateDate = 
+var fecha = prompt("Ingresa FECHA");
+var validateDate;
+var fechaOk = false;
+var email = prompt("Ingresa CORREO");
+var validateEmail = /^([.-]|\w)+@([.-]|\w)+(\.\w{2,3})+$/;
+var emailOk = false;
 
-function valida(){
+var expre = /^\d{1,2}\/\d{1,2}\/\d{4}$/;//EXPRESIÓN REGULR QUE VALIDA EL FORMATO FECHA
 
+function valida(fecha){
+    if(fecha != null && fecha.match(expre)) {
+        fechaOk = true;
+    }
+    return fechaOk;
 }
