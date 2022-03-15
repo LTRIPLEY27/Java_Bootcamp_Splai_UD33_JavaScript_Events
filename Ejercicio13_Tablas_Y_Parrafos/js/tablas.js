be34e0e6c066501b90c3b2c165a109168170ded0
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-03-15 21:17:14 +0100

    1

--- Ejercicio13_Tablas_Y_Parrafos/js/tablas.js
+++ Ejercicio13_Tablas_Y_Parrafos/js/tablas.js
@@ -1,8 +1,8 @@
 function pushing(x) {
-    let valor = x.id;
+    let valor = x.id; //OBTENCIÓN DEL ID DEL EVENTO
     if(valor == "tabla2") {
         alert("Se presionó un párrafo contenido en la segunda tabla");
     } else {
-        alert("Se presionó un párrafo del documentogit ");
+        alert("Se presionó un párrafo del documento ");
     }
 }
---
+++ Ejercicio15_visor/js/visor.js
@@ -0,0 +1,7 @@
+function visualiza(img) {//LAS IMÁGENES SE PUEDEN PASAR CON SUS ATRIBUTOS EN JAVASCRIPT
+    images = "img/foto" + img + ".jpg";//COMPONE LA IMAGEN EXACTA AGRAGANDO CON STRINGS EL NUMERO
+    document.images["foto"].src = images;
+    var name = document.images.src.alt;
+    alert(hola);
+    document.getElementById("nombre")+= "hola";
+}
